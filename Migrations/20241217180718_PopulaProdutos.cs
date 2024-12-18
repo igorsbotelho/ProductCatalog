63ec9aefe61926bd0e06ba4f0b8d445b72721774
Igor Botelho <igorbotelho65@gmail.com>
2024-12-17 22:59:38 -0300

    ProductDevelopment

---
+++ Migrations/20241217180718_PopulaProdutos.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaProdutos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "Values ('Produto 1', 'Descrição do Produto 1', 100.00, 'imagem.jpg', 50.0, now(), 3)");
+
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "Values ('Produto 2', 'Descrição do Produto 2', 200.00, 'imagem.jpg', 30.0, now(), 3)");
+
+            mb.Sql("Insert into Produtos (Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) " +
+                   "Values ('Produto 3', 'Descrição do Produto 3', 150.00, 'imagem.jpg', 20.0, now(), 4)");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Produtos");
+        }
+    }
+}
---
+++ Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualBasic;
+
+namespace APICatalogo.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
+    public int CategoriaId { get; set; } // EF entende como PK
+    [Required]
+    [StringLength(80)]
+    public string? Nome { get; set; }
+    [Required]
+    [StringLength(300)]
+    public string? ImagemUrl { get; set; }
+        
+    public ICollection<Produto>? Produtos { get; set; }
+}
